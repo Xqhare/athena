5db65a0c981243042e8734e60b8f7ee235590c74
Xqhare <xqhare@web.de>
2025-03-23 23:14:31 +0100

    feat(byte_bit/reader+writer): add reader and writer for io::Reader and io::Writer, add tests for them, add doc for them

--- src/tools/byte_bit/mod.rs
+++ src/tools/byte_bit/mod.rs
@@ -1,5 +1,9 @@
 pub mod decoder;
 pub mod encoder;
+pub mod reader;
+pub mod writer;
 
 pub use decoder::byte_bit_decoder;
 pub use encoder::byte_bit_encoder;
+pub use reader::byte_bit_reader;
+pub use writer::byte_bit_writer;
