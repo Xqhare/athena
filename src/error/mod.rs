6976516f924f516e37e511819efc40b07de5bc79
Xqhare <xqhare@web.de>
2025-05-06 00:18:41 +0200

    feat(error): add Overflow; add Display implementation

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -6,4 +6,20 @@ pub enum AthenaError {
     InsufficientBuffer,
     IoError(std::io::Error),
     ContinuationBitInLastByte,
+    Overflow,
 }
+
+impl std::fmt::Display for AthenaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for AthenaError {}
+
+impl From<std::io::Error> for AthenaError {
+    fn from(e: std::io::Error) -> Self {
+        AthenaError::IoError(e)
+    }
+}
+
