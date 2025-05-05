a2672ac0c5e826439b4e76db931e0a724cb9d909
Xqhare <xqhare@web.de>
2025-05-06 00:19:08 +0200

    fix(lib): reexports for convinience

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 mod error;
-pub mod utils;
-pub mod tools;
+mod utils;
+mod tools;
+
+// Reexports for convenience
+
+pub mod compression {
+    pub use crate::utils::compression::lzw::{compress_lzw, decompress_lzw};
+    pub use crate::utils::compression::delta::{delta_encoder, delta_decoder};
+    pub use crate::utils::compression::run_length::{run_length_encoder, run_length_decoder};
+}
+pub mod checksum {
+    pub mod crc32 {
+        pub use crate::tools::checksum::crc32::*;
+    }
+}
+pub mod encoding {
+    pub mod leb128 {
+        pub use crate::tools::leb128::*;
+    }
+}
+pub mod byte_bit {
+    pub use crate::tools::byte_bit::*;
+}
