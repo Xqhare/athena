68ad4585166e2254c57da93a329d830b5d9dfdf0
Xqhare <xqhare@web.de>
2025-05-06 23:27:55 +0200

    fix(lib): fix reexports

--- src/lib.rs
+++ src/lib.rs
@@ -16,5 +16,8 @@ pub mod encoding_and_decoding {
     pub use crate::tools::run_length::*;
 }
 pub mod byte_bit {
-    pub use crate::tools::byte_bit::*;
+    pub use crate::tools::byte_bit::reader::byte_bit_reader;
+    pub use crate::tools::byte_bit::writer::byte_bit_writer;
+    pub use crate::tools::byte_bit::decoder::byte_bit_decoder;
+    pub use crate::tools::byte_bit::encoder::byte_bit_encoder;
 }
