9177fb8b603a6f9106562f9d203559ab3fac5359
Xqhare <xqhare@web.de>
2025-05-20 21:57:52 +0200

    test(lzw): add regression test

--- src/utils/compression/lzw/tests.rs
+++ src/utils/compression/lzw/tests.rs
@@ -1,5 +1,13 @@
 #![allow(unused_imports)]
-use crate::compression::{compress_lzw, decompress_lzw};
+use crate::compression::{compress_lzw, compress_lzw_encode_leb128, decompress_lzw, decompress_lzw_decode_leb128};
+
+#[test]
+fn used_to_panic_incorrect_decoding() {
+    let data = vec![103];
+    let compressed = compress_lzw_encode_leb128(&data);
+    let decompressed = decompress_lzw_decode_leb128(&compressed).unwrap();
+    assert_eq!(String::from_utf8(decompressed).unwrap(), String::from_utf8(data).unwrap());
+}
 
 #[test]
 fn compression_check() {
