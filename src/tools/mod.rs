7a46032930dbfed48164855878a592134861ed28
Xqhare <xqhare@web.de>
2025-05-24 01:48:14 +0200

    chore(feature flags): try to make them actually work as intended

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,4 +1,8 @@
-pub mod byte_bit;
+#[cfg(any(doc, feature = "encoding_decoding"))]
 pub mod leb128;
+#[cfg(any(doc, feature = "encoding_decoding"))]
 pub mod run_length;
+#[cfg(any(doc, feature = "encoding_decoding"))]
 pub mod delta;
+#[cfg(any(doc, feature = "byte_bit"))]
+pub mod byte_bit;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(any(doc, feature = "compression"))]
 pub mod compression;
+#[cfg(any(doc, feature = "checksum"))]
 pub mod checksum;
+#[cfg(any(doc, feature = "traits"))]
 pub mod traits;
