80b6c65e8a863fb2e6cc60818c5610506d7d27a1
Xqhare <xqhare@web.de>
2026-03-28 03:07:33 +0100

    fix test

--- src/utils/compression/lzw/tests.rs
+++ src/utils/compression/lzw/tests.rs
@@ -24,13 +24,6 @@ fn compression_check() {
     assert!(compressed.len() < len);
 
     let (data, len) = {
-        let tmp = std::fs::read_to_string("Session.vim").unwrap();
-        (tmp.as_bytes().to_vec(), tmp.len())
-    };
-    let compressed = compress_lzw(&data);
-    assert!(compressed.len() < len);
-
-    let (data, len) = {
         let tmp = std::fs::read_to_string("README.md").unwrap();
         (tmp.as_bytes().to_vec(), tmp.len())
     };
@@ -56,14 +49,6 @@ fn compression_check() {
 fn compression_decompression() {
     let (data, string) = {
         let tmp = std::fs::read_to_string("src/utils/compression/lzw/mod.rs").unwrap();
-        (tmp.as_bytes().to_vec(), tmp)
-    };
-    let compressed = compress_lzw(&data);
-    let decompressed = decompress_lzw(&compressed);
-    assert_eq!(String::from_utf8(decompressed).unwrap(), string);
-
-    let (data, string) = {
-        let tmp = std::fs::read_to_string("Session.vim").unwrap();
         (tmp.as_bytes().to_vec(), tmp)
     };
     let compressed = compress_lzw(&data);
